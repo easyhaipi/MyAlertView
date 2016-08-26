1ba882a9f46367068df84b2b2c833e04757492d4
easyhaipi <easy_qingsong@163.com>
2016-08-26 15:46:11 +0800

    Initial Commit

---
+++ MyAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyAlertView
+//
+//  Created by taobaichi on 16/8/26.
+//  Copyright © 2016年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAlertView
+//
+//  Created by taobaichi on 16/8/26.
+//  Copyright © 2016年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
